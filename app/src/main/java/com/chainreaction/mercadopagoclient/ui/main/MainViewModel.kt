93ba05dc10c57f930b6e913b43fa235ed81750f3
Denis Reinoso <reinoso.denis@gmail.com>
2020-06-16 18:40:52 -0300

    Add amount input and navigation

--- app/src/main/java/com/chainreaction/mercadopagoclient/ui/main/MainViewModel.kt
+++ app/src/main/java/com/chainreaction/mercadopagoclient/ui/main/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.chainreaction.mercadopagoclient.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var amountFormattedLiveData = MutableLiveData<String>()
+
 }
