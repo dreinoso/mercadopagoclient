d396ea2f7aa4582c524b66aa1f05301444df69a6
Denis Reinoso <reinoso.denis@gmail.com>
2020-06-18 07:47:21 -0300

    Add divider to installments

--- app/src/main/java/com/chainreaction/mercadopagoclient/ui/main/InstallmentFragment.kt
+++ app/src/main/java/com/chainreaction/mercadopagoclient/ui/main/InstallmentFragment.kt
@@ -2,17 +2,19 @@ package com.chainreaction.mercadopagoclient.ui.main
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.chainreaction.mercadopagoclient.databinding.FragmentInstallmnentListBinding
 import com.chainreaction.mercadopagoclient.model.Entity
+
 
 class InstallmentFragment : Fragment() {
 
@@ -42,6 +44,11 @@ class InstallmentFragment : Fragment() {
                 adapter = InstallmentRecyclerViewAdapter(installments as List<Entity.Installment>)
                 binding.list.adapter = adapter
                 binding.list.layoutManager = LinearLayoutManager(context)
+                val dividerItemDecoration = DividerItemDecoration(
+                    binding.list.context,
+                    (binding.list.layoutManager as LinearLayoutManager).orientation
+                )
+                binding.list.addItemDecoration(dividerItemDecoration)
                 adapter.notifyDataSetChanged()
                 showSuccessfulDialog()
             }
